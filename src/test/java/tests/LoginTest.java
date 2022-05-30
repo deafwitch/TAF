879c2e9da99b0d15577f3a8ee32be6d739f721e7
Rudakouski_V <rva1997@gmail.com>
2022-05-30 22:05:48 +0300

    Commit after the whole lesson

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,10 +3,13 @@ package tests;
 import baseEntities.BaseTest;
 import configuration.ReadProperties;
 import io.qameta.allure.Allure;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import wrappers.RadioButton;
+import wrappers.Table;
 
 import java.io.ByteArrayInputStream;
 import java.util.UUID;
@@ -45,6 +48,34 @@ public class LoginTest extends BaseTest {
                 loginStep.incorrectLogin(ReadProperties.username(), "123").getErrorTextElement().getText(),
                 "Email/Login or Password is incorrect. Please try again.",
                 "Неверное сообщение об ошибке");
+    }
+
+    @Test
+    public void radioButtonTest() throws Exception {
+        loginStep.successLogin(
+                ReadProperties.username(),
+                ReadProperties.password()
+        );
+
+        driver.get("https://aqa1803.testrail.io/index.php?/admin/projects/add");
+
+        RadioButton suiteMode = new RadioButton(driver, By.name("suite_mode"));
+        suiteMode.select("2");
+
+        Thread.sleep(4000);
+    }
+
+    @Test
+    public void tablesTest() {
+        loginStep.successLogin(
+                ReadProperties.username(),
+                ReadProperties.password()
+        );
+
+        driver.get("https://aqa1803.testrail.io/index.php?/admin/projects/overview");
+
+        Table table = new Table(driver, By.cssSelector("table.grid"));
+        System.out.println(table.getColumnIndex("Project"));
     }
 }
 
