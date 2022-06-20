fe6198d18bc9fdf7023fc6d9eb788880ca699f4a
nikita <100132643+deafwitch@users.noreply.github.com>
2022-06-20 21:52:42 +0300

    Commit before a lesson

--- src/test/java/tests/db/BaseDBTest.java
+++ src/test/java/tests/db/BaseDBTest.java
@@ -1,5 +1,6 @@
 package tests.db;
 
+import dbEntities.CustomersTable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterTest;
@@ -7,9 +8,11 @@ import org.testng.annotations.BeforeTest;
 import services.DataBaseService;
 
 public class BaseDBTest {
-    Logger logger = LoggerFactory.getLogger(BaseDBTest.class);
-
     public DataBaseService dataBaseService;
+
+    CustomersTable customersTable;
+
+    Logger logger = LoggerFactory.getLogger(BaseDBTest.class);
 
     public BaseDBTest() {
     }
@@ -17,6 +20,14 @@ public class BaseDBTest {
     @BeforeTest
     public void setupConnection() {
         dataBaseService = new DataBaseService();
+
+        customersTable = new CustomersTable(dataBaseService);
+        customersTable.dropTable();
+        customersTable.createCustomersTable();
+
+        customersTable.addCustomer("Evan", "Perks", "ivanov@test.com", 28);
+        customersTable.addCustomer("Evan1", "Evan2", "petrov@test.com", 38);
+        customersTable.addCustomer("Evan23", "Evaneewefwef", "marina@test.com", 23);
     }
 
     @AfterTest
